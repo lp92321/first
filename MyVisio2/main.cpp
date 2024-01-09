a889b571ed529b68f01057bd51a1b68e2a017d91
pengliu <connect_liupeng@163.com>
2024-01-09 20:46:02 +0800

    代码变更提交测试

--- MyVisio2/main.cpp
+++ MyVisio2/main.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
+    MainWindow w;
+    w.show();
 
     //有个bug,连接线移动中间的节点，不停的闪或者超过边界了
     //内存泄漏
